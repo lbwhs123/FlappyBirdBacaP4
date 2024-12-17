898d1922314ad8e878aa237c4d524ec1d7f477db
lbwhs123 <lylabaca2028@gmail.com>
2024-12-17 12:40:34 -0800

    finshed
    finshed the flappy bridd final

--- FlappyBirdBacaP4/Assets/Scripts/GameControl.cs
+++ FlappyBirdBacaP4/Assets/Scripts/GameControl.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 using TMPro;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameObject gameOverTextTMPro;
+    public TextMeshProUGUI ScoreText;
     public GameObject DeathText;
     public bool gameOver = false;
     public float scrollSpeed = -1.5f;
 
+    private int score = 0;
     // Start is called before the first frame update
     void Awake()
     {
@@ -33,7 +36,15 @@ public class GameControl : MonoBehaviour
             SceneManager.LoadScene(SceneManager.GetActiveScene ().buildIndex);
         }
     }
-
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        ScoreText.text = "Score: " + score.ToString (); 
+    }
     public void BridDied()
     {
         gameOverTextTMPro.SetActive (true);
